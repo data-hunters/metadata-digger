e52c34ead3cdcd2ab0b19fc0fa85221f2dabb329
sauermad <dawidsau@gmail.com>
2020-04-18 23:28:01 +0200

    Issue #40 Small fix

--- src/main/scala/ai/datahunters/md/schema/MultiLabelPredictionSchemaConfig.scala
+++ src/main/scala/ai/datahunters/md/schema/MultiLabelPredictionSchemaConfig.scala
@@ -2,7 +2,7 @@ package ai.datahunters.md.schema
 import ai.datahunters.md.schema.BinaryInputSchemaConfig.{BasePathCol, FileCol, FilePathCol, IDCol}
 import org.apache.spark.sql.types.StructType
 
-object MultiLabelPredictionSchemaConfig extends SchemaConfig {
+class MultiLabelPredictionSchemaConfig(hashList: Seq[String] = Seq()) extends SchemaConfig {
 
   val LabelsCol = "Labels"
   /**
@@ -29,19 +29,7 @@ object MultiLabelPredictionSchemaConfig extends SchemaConfig {
     .addStringField(FilePathCol)
     .addBinaryField(FileCol)
     .addStringArrayField(LabelsCol)
-    .build()
-
-  /**
-    * Overloaded method add all hash columns additionally.
-    * @param hashList
-    * @return
-    */
-  def schema(hashList: Seq[String] = Seq()): StructType = new SchemaBuilder()
-    .addStringField(IDCol)
-    .addStringField(BasePathCol)
-    .addStringField(FilePathCol)
-    .addBinaryField(FileCol)
-    .addStringArrayField(LabelsCol)
     .addStringFields(hashList)
     .build()
+
 }
